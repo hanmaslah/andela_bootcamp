719d30d5213d97513b0c8f85b4e6bf760bb234ec
Hannah Masila <hannahmasila@gmail.com>
2016-06-13 22:01:39 +0300

    This prints a number and whether it is prime or not

--- prime.py
+++ prime.py
@@ -1,11 +1,11 @@
 def prime(k):
-	
-		for n in range (2,k):
-			if k>1:
-				for i in range (2,n):
-					if n%i==0:
-						print (n, "is not prime")
-						print(i, "times", n//i, "is", n)
-						break
-				else:
-					print(n, "is prime")
+	if k>1:
+		for i in range(2,k):
+			if k%i==0:
+				print(k, "is not prime")
+				break
+		else:
+			print(k, "is prime")
+	else:
+		return None 
+	prime(k-1)
