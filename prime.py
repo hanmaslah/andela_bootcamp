857493c730cd341b0480abd9a6369f427b51d687
Hannah Masila <hannahmasila@gmail.com>
2016-06-14 11:04:24 +0300

    Prints the numbers on the range given and state whether it is a prime number or not

--- prime.py
+++ prime.py
@@ -6,10 +6,10 @@ and prints whether prime or not prime
 	if k>1:
 		for i in range(2,k):
 			if k%i==0:
-				print(k, "is not prime")
+				print(k, "is not a prime number")
 				break
 		else:
-			print(k, "is prime")
+			print(k, "is a prime number")
 	else:
 		return None 
 	prime(k-1)
