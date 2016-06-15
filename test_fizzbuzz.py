e3a1165100453af2ed524f0719e1a923b12c42e5
Hannah Masila <hannahmasila@gmail.com>
2016-06-15 23:44:24 +0300

    fizzbuzz testcase that tests if divisibility by 3 returns fizz, divisibility by 5 returns buzz and divisibility by 5 and 3 fizzbuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -11,28 +11,28 @@ Testing FizzBuzz
         """
 Test returns fizz when input is divisible by 3
         """
-        self.assertEqual(fizzbuzz.fizz_buzz(3),'fizz')
+        self.assertEqual(fizzbuzz.fizz_buzz(9),'fizz')
 
     def test_returns_buzz_divisibility_by_five(self):
          """
 Test returns buzz when input is divisible by 5
 
          """
-         self.assertEqual(fizzbuzz.fizz_buzz(5), 'buzz')
+         self.assertEqual(fizzbuzz.fizz_buzz(25), 'buzz')
          
     def test_returns_fizzbuzz_divisibility_by_five_and_three(self):
           """
 Test returns fizzbuzz when input is divisible by both 3 and 5
           """
-          self.assertEqual(fizzbuzz.fizz_buzz(15), 'fizzbuzz')
+          self.assertEqual(fizzbuzz.fizz_buzz(45), 'fizzbuzz')
     def test_returns_number_if_not_divisible_by_five_or_three(self):
           """
 Test returns number when input is divisible by both 3 and 5
           """
-          self.assertEqual(fizzbuzz.fizz_buzz(7), 7)      
+          self.assertEqual(fizzbuzz.fizz_buzz(11), 11)      
     
     def test_returns_error(self):
           """
 Test returns fizzbuzz when input is divisible by both 3 and 5
           """
-          self.assertEqual(fizzbuzz.fizz_buzz('e'), 'Kindly enter a digit')   
+          self.assertEqual(fizzbuzz.fizz_buzz('name'), 'Kindly enter a digit')   
