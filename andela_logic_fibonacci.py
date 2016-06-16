c2ff2f0ca9625afeb2f94b0b5e167aabfe9a34c1
Hannah Masila <hannahmasila@gmail.com>
2016-06-16 12:37:00 +0300

    adds fibonacci Fibonacci series from 0 to the nth term

--- andela_logic_fibonacci.py
+++ andela_logic_fibonacci.py
@@ -4,7 +4,7 @@ A Fibonacci sequence is the integer sequence of 1, 1, 2, 3, 5, 8....
 The terms are obtained by adding the preceding two terms.
 This means to say the nth term is the sum of (n-1)th and (n-2)th term.
         """	
-        a,b = 0,1
+        a,b = 1,0
         print("Fibonacci series of",n,"terms is: ")
         for i in range(n):
 				
